41b5080c10bb7aafb1769f77de3d2d1a19a8a291
igoshev-konstantin <leader_trident@mail.ru>
2020-09-16 14:39:17 +0300

    Second commit

--- src/main/java/Spring/annotations/Annotations.java
+++ src/main/java/Spring/annotations/Annotations.java
@@ -1,6 +1,8 @@
 package Spring.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,12 @@ public class Annotations {
             new ClassPathXmlApplicationContext("applicationContextAnnotations.xml");
 
     public static void main(String[] args) {
-      //  newCarFactory();
+        //  newCarFactory();
         newOrder();
+        moveCarIntoWarehouse();
+
+
+        context.close();
     }
 
     public static void newCarFactory() {
@@ -21,35 +27,22 @@ public class Annotations {
         CarsType carType = CarsType.values()[RandomTypeNumber];
         CarsFactory carsFactory = context.getBean("carsFactory", CarsFactory.class);
         System.out.println("Made new car: " + carsFactory.MadeCar(carType).getCarType());
-        context.close();
     }
 
     public static void newOrder() {
         Customer customer = context.getBean("customer", Customer.class);
-        System.out.println("Ordered car:"+customer.getOrderedCar().getCarType());
-        context.close();
+        System.out.println("Ordered car:" + customer.getOrderedCar().getCarType()+" Car:"+customer.getOrderedCar());
+    }
+
+    public static void moveCarIntoWarehouse() {
+        Warehouse warehouse = context.getBean("warehouse", Warehouse.class);
+        System.out.println("Car owner: " + warehouse.getCarOwner() + " Warehouse: " + warehouse.getCar());
     }
 }
 
 @Component
-class CarsFactory {
-
-    public Car MadeCar(CarsType carsType) {
-
-        switch (carsType) {
-            case TOYOTA:
-                return new Car(CarsType.TOYOTA);
-            case HONDA:
-                return new Car(CarsType.HONDA);
-            case NISSAN:
-                return new Car(CarsType.NISSAN);
-        }
-        return null;
-    }
-}
-
-@Component
-class Car implements CarsInterface{
+@Scope("prototype")
+class Car implements CarsInterface {
     private CarsType carType;
 
     public Car() {
@@ -67,11 +60,13 @@ class Car implements CarsInterface{
 
 }
 
-//@Component    //Если 2 одинаковых бина, то при Autowired Spring-у будет непонятно какой из них выбрать
-class Truck implements CarsInterface{
+@Component //Если 2 одинаковых бина, то при Autowired Spring-у будет непонятно какой из них выбрать
+class Truck implements CarsInterface {
     private CarsType carType;
 
-    public Truck() {}
+    public Truck() {
+        this.carType = CarsType.values()[3];
+    }
 
     public CarsType getCarType() {
         return carType;
@@ -80,25 +75,51 @@ class Truck implements CarsInterface{
 
 @Component
 class Customer {
+
     private final CarsInterface orderedCar;
 
     @Autowired
-    public Customer(CarsInterface orderedCar) {
+    public Customer(@Qualifier("truck") CarsInterface orderedCar) { //Пишем @Qualifier сразу в параметре
         this.orderedCar = orderedCar;
     }
-    public CarsInterface getOrderedCar(){
+
+    public CarsInterface getOrderedCar() {
         return orderedCar;
     }
 }
-//@Component
-//class Customer {
-//    private final Car orderedCar;
-//
-//    @Autowired
-//    public Customer(Car orderedCar) {
-//        this.orderedCar = orderedCar;
-//    }
-//    public Car getOrderedCar(){
-//        return orderedCar;
-//    }
-//}
+
+@Component
+class Warehouse {
+    private CarsInterface car;
+    private Customer carOwner;
+
+    @Autowired
+    public Warehouse(@Qualifier("truck") CarsInterface car, Customer carOwner) {
+        this.car = car;
+        this.carOwner = carOwner;
+    }
+
+    public CarsInterface getCar() {
+        return car;
+    }
+
+    public Customer getCarOwner() {
+        return carOwner;
+    }
+}
+@Component
+class CarsFactory {
+
+    public Car MadeCar(CarsType carsType) {
+
+        switch (carsType) {
+            case TOYOTA:
+                return new Car(CarsType.TOYOTA);
+            case HONDA:
+                return new Car(CarsType.HONDA);
+            case NISSAN:
+                return new Car(CarsType.NISSAN);
+        }
+        return null;
+    }
+}
